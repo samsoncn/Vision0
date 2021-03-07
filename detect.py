7c7b004f156a2f10cca643ea6f2ff64f6171da0c
samsonc <samsonc0514@gmail.com>
2021-03-07 01:56:08 -0500

    Updated car detection

--- detect.py
+++ detect.py
@@ -6,20 +6,31 @@ import imutils
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+classifier_file = 'cars.xml'
+
+car_tracker = cv2.CascadeClassifier(classifier_file)
+
 def detector(frame):
-    frame = imutils.resize(frame, width=min(600, frame.shape[1]))
-    rect, weight = hog.detectMultiScale(frame, winStride=(6, 6), padding=(16, 16), scale=1.3)
+    rect, weight = hog.detectMultiScale(frame, winStride=(6, 8), padding=(3, 3), scale=1.15)
+    # rect, weight = hog.detectMultiScale(frame, winStride=(2, 6), padding=(6, 6), scale=2)
+
+    cars = car_tracker.detectMultiScale(frame)
+
     rect = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rect])
     label = non_max_suppression(rect, probs=None, overlapThresh=0.65)
-
+    
     c = 0
     for (x, y, w, h) in label:
         c += 1
 
         cv2.rectangle(frame, (x, y), (w, h), (139, 30, 90), 2)
-        cv2.rectangle(frame, (x, y - 20), (w, y), (139, 30, 90), -1)
-        cv2.putText(frame, f'Person{c}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.rectangle(frame, (x, y - 15), (w, y), (139, 30, 90), -1)
+        cv2.putText(frame, f'Person{c}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
+        cv2.rectangle(frame, (x, y - 15), (x+w, y), (0, 0, 255), -1)
+        cv2.putText(frame, f'Car{c}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
-    #cv2.imshow('Pedstrians', frame)
+    cv2.imshow('Vision 0', frame)
     return frame, c
-
--- video_reader.py
+++ video_reader.py
@@ -1,14 +1,17 @@
 import cv2
+import imutils
 from detect import detector
 
-cap = cv2.VideoCapture('./testing/t3.mp4')
+cap = cv2.VideoCapture('./testing/video.mp4')
 
 while (cap.isOpened()):
     ret, frame = cap.read()
     # print(frame)
     # print(fps)
+    frame = imutils.resize(frame, width=min(800, frame.shape[1]))
     frame = detector(frame)
 
+    # When user click 'enter', it will stop
     if cv2.waitKey(1) == 13:
         break
     if frame is None:
